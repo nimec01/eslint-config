7434e737508e7a07206351e2bd30da9f982f3156
Nick <24428341+Nick46000@users.noreply.github.com>
2023-06-03 11:46:42 +0200

    add react-typescript config

--- react-typescript.js
+++ react-typescript.js
@@ -1,3 +1,39 @@
-// TODO replace with config for react + typescript
+const {
+  base: basePlugins,
+  react: reactPlugins,
+  typescript: typescriptPlugins,
+  alwaysLastTypescript: alwaysLastTypescriptPlugins,
+} = require('./lib/plugins');
+const {
+  base: baseConfigs,
+  react: reactConfigs,
+  typescript: typescriptConfigs,
+  alwaysLastTypescript: alwaysLastTypescriptConfigs,
+} = require('./lib/configs');
+const {
+  base: baseRules,
+  react: reactRules,
+  typescript: typescriptRules,
+} = require('./lib/rules');
 
-module.exports = require('./base');
+module.exports = {
+  parser: '@typescript-eslint/parser',
+
+  extends: [
+    ...baseConfigs,
+    ...reactConfigs,
+    ...typescriptConfigs,
+    ...alwaysLastTypescriptConfigs,
+  ],
+  plugins: [
+    ...basePlugins,
+    ...reactPlugins,
+    ...typescriptPlugins,
+    ...alwaysLastTypescriptPlugins,
+  ],
+  rules: {
+    ...baseRules,
+    ...reactRules,
+    ...typescriptRules,
+  },
+};
