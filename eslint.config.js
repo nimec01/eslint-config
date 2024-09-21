096bdd3d2f7d2d53481714b195765435833a25aa
nimec01 <24428341+nimec01@users.noreply.github.com>
2024-09-21 20:10:54 +0200

    fix: followup

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,3 @@
 import baseConfig from './src/base.js';
 
-export default [...baseConfig, { ignores: ['**/tests/base'] }];
+export default [...baseConfig, { ignores: ['**/tests/base', '**/dist'] }];
