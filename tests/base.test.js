db6f7be1e95fb05689be4a8f9117b9fd3cde6cc6
nimec01 <24428341+nimec01@users.noreply.github.com>
2024-09-21 18:56:20 +0200

    style: format

--- tests/base.test.js
+++ tests/base.test.js
@@ -1,16 +1,15 @@
-import { expect, test } from "@jest/globals";
-import { ESLint } from "eslint";
-import config from "../src/index";
+import { expect, test } from '@jest/globals';
+import { ESLint } from 'eslint';
+import config from '../src/index';
 
-test("base flavor should be linted correctly", async () => {
-
+test('base flavor should be linted correctly', async () => {
   const eslint = new ESLint({
     overrideConfigFile: true,
     overrideConfig: config.configs.base,
-    fix: false
+    fix: false,
   });
 
-  const results = await eslint.lintFiles(["tests/base"]);
+  const results = await eslint.lintFiles(['tests/base']);
 
   expect(results).toBeDefined();
   expect(results.length).toBe(2);
@@ -19,93 +18,91 @@ test("base flavor should be linted correctly", async () => {
   expect(results.at(0).messages).toEqual(
     expect.arrayContaining([
       expect.objectContaining({
-        ruleId: "prettier/prettier",
+        ruleId: 'prettier/prettier',
         line: 1,
-        column: 1
+        column: 1,
       }),
       expect.objectContaining({
-        ruleId: "no-unused-vars",
+        ruleId: 'no-unused-vars',
         line: 2,
-        column: 7
+        column: 7,
       }),
       expect.objectContaining({
-        ruleId: "prettier/prettier",
+        ruleId: 'prettier/prettier',
         line: 4,
-        column: 22
+        column: 22,
       }),
       expect.objectContaining({
-        ruleId: "no-console",
+        ruleId: 'no-console',
         line: 6,
-        column: 1
+        column: 1,
       }),
       expect.objectContaining({
-        ruleId: "prettier/prettier",
+        ruleId: 'prettier/prettier',
         line: 6,
-        column: 13
+        column: 13,
       }),
       expect.objectContaining({
-        ruleId: "prettier/prettier",
+        ruleId: 'prettier/prettier',
         line: 10,
-        column: 3
+        column: 3,
       }),
       expect.objectContaining({
-        ruleId: "no-secrets/no-secrets",
+        ruleId: 'no-secrets/no-secrets',
         line: 10,
-        column: 3
+        column: 3,
       }),
       expect.objectContaining({
-        ruleId: "@cspell/spellchecker",
+        ruleId: '@cspell/spellchecker',
         line: 10,
-        column: 11
+        column: 11,
       }),
       expect.objectContaining({
-        ruleId: "@cspell/spellchecker",
+        ruleId: '@cspell/spellchecker',
         line: 10,
-        column: 26
+        column: 26,
       }),
       expect.objectContaining({
-        ruleId: "@cspell/spellchecker",
+        ruleId: '@cspell/spellchecker',
         line: 10,
-        column: 52
+        column: 52,
       }),
     ])
   );
-
 
   expect(results.at(1).errorCount).toBe(6);
   expect(results.at(1).messages).toEqual(
     expect.arrayContaining([
       expect.objectContaining({
-        ruleId: "prettier/prettier",
+        ruleId: 'prettier/prettier',
         line: 1,
-        column: 1
+        column: 1,
       }),
       expect.objectContaining({
-        ruleId: "import-x/default",
+        ruleId: 'import-x/default',
         line: 3,
-        column: 8
+        column: 8,
       }),
       expect.objectContaining({
-        ruleId: "no-unused-vars",
+        ruleId: 'no-unused-vars',
         line: 3,
-        column: 8
+        column: 8,
       }),
       expect.objectContaining({
-        ruleId: "sonarjs/unused-import",
+        ruleId: 'sonarjs/unused-import',
         line: 3,
-        column: 8
+        column: 8,
       }),
       expect.objectContaining({
-        ruleId: "unicorn/no-process-exit",
+        ruleId: 'unicorn/no-process-exit',
         line: 5,
-        column: 1
+        column: 1,
       }),
       expect.objectContaining({
-        ruleId: "prettier/prettier",
+        ruleId: 'prettier/prettier',
         line: 6,
-        column: 1
+        column: 1,
       }),
     ])
   );
-  
 });
